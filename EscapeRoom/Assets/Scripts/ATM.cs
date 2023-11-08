881af89910acfd81c68f5a43bd71d1961f062f76
mateomal2608 <teomaldonado2608@gmail.com>
2023-11-08 13:27:33 -0500

    New ATMs+ Interactions

--- EscapeRoom/Assets/Scripts/ATM.cs
+++ EscapeRoom/Assets/Scripts/ATM.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ATM : MonoBehaviour
 {
     public GameObject atmScreen;
+    myControls inputActions;
+    public UnityEvent myAction;
+
+
+    private void Awake()
+    {
+        inputActions=new myControls();
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
             Debug.Log("Enter the space");
-
+            LeanTween.scale(atmScreen, Vector3.one, 2).setEaseInBounce();
             
         }
 
@@ -22,8 +32,31 @@ public class ATM : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("Leave the space");
+            LeanTween.scale(atmScreen, Vector3.zero, 2).setEaseInQuad();
+
         }
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            if(inputActions.Player.ActionKey.WasPerformedThisFrame())
+            {
+                myAction.Invoke();
+            }
 
+
+        }
+    }
+
+    public void OnEnable()
+    {
+        inputActions.Player.Enable();
+    }
+
+    public void OnDisable()
+    {
+        inputActions.Player.Disable();
+    }
 }
