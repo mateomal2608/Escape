c99209753bdb9e45ab3d9bd0ed3ee4e47d0f4cf8
mateomal2608 <teomaldonado2608@gmail.com>
2023-11-09 18:10:58 -0500

    New Fireplace Screen

---
+++ EscapeRoom/Assets/Scripts/FireplaceScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireplaceScript : MonoBehaviour
+{
+    public GameObject fireplaceScreen;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Enter the space");
+            LeanTween.scale(fireplaceScreen, Vector3.one, 2);
+
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Leave the space");
+            LeanTween.scale(fireplaceScreen, Vector3.zero, 2).setEaseInQuad();
+
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
